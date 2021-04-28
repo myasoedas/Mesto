2a80c28f31cb879d14d0c96db4f044ef14ae1879
myasoedas <myasoedas@yandex.ru>
2021-04-28 20:40:59 +0300

    fix: Исправил ошибки в файле index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,44 +1,48 @@
-/* Установить слушатель на событие clic по кнопке button*/
+/*Использовать const при объявлении переменной, которая не меняется за все время работы со страницей index.html*/
+/*Использовать let при объявлении переменной, значение которой будет изменяться во время работы со страницей index.html*/
 const buttonEdit = document.querySelector('.profile__button-edit');
 const buttonClose = document.querySelector('.popup__button-close');
 const buttonSave = document.querySelector('.form__button-save');
 
+const title = document.querySelector('.profile__title');
+const text = document.querySelector('.profile__text');
+
+const fieldName = document.querySelector('.form__field_name');
+const fieldCaption = document.querySelector('.form__field_caption');
+
+const overlay = document.querySelector('.overlay');
+
+/*включить: display: grid в блоке overlay*/
+function overlayToggle() {
+  overlay.classList.toggle('overlay_is-opened');
+}
+
+/*Открыть popup и записать в поля формы name и caption - текстовые значения из блока profile*/
 function openPopup() {
-  let title = document.querySelector('.profile__title');
-  let text = document.querySelector('.profile__text');
+  fieldName.value = title.textContent;
+  fieldCaption.value = text.textContent;
 
-  captionEdit = document.querySelectorAll('.form__field');
-  captionEdit[0].setAttribute('value', title.textContent);
-  captionEdit[1].setAttribute('value', text.textContent);
-
-  overlay = document.querySelector('.overlay');
-  overlay.classList.toggle('overlay_is-opened');
+  overlayToggle();
 }
 
+/*Закрыть popup*/
 function closePopup() {
-  overlay = document.querySelector('.overlay');
-  overlay.classList.toggle('overlay_is-opened');
+  overlayToggle();
 }
 
+/*Записать данные введенные пользователем в полях name и caption формы в соответсвующие текстовые поля в блоке profile*/
 function saveForm(event) {
   event.preventDefault();
 
-  let title = document.querySelector('.profile__title');
-  let text = document.querySelector('.profile__text');
+  title.textContent = fieldName.value;
+  text.textContent = fieldCaption.value;
 
-  captionEdit = document.querySelectorAll('.form__field');
-  title.textContent = captionEdit[0].value;
-  text.textContent = captionEdit[1].value;
-
-  overlay = document.querySelector('.overlay');
-  overlay.classList.toggle('overlay_is-opened');
+  overlayToggle();
 }
 
-
-buttonEdit.addEventListener('click', openPopup);
-
-buttonClose.addEventListener('click', closePopup);
-
-buttonSave.addEventListener('click', saveForm);
+/*подключить слушатели к кнопкам*/
+buttonEdit.addEventListener('click', openPopup); /*при нажатии на кнопку button-edit - выполнить функцию openPopup()*/
+buttonClose.addEventListener('click', closePopup); /*при нажатии на кнопку button-close - выполнить функцию closePopup()*/
+buttonSave.addEventListener('click', saveForm); /*при нажатии на кнопку button-save - выполнить функцию saveForm()*/
 
 
