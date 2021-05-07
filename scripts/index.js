fc60a8e476b3ba109ad37005a6765fd0e8f46883
myasoedas <myasoedas@yandex.ru>
2021-05-07 15:44:14 +0300

    feat: Реализовал добавление шести карточек на страницу через перебор массива в JS и тег template

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,52 @@
+const arrPopupFormFields = [
+  {
+    popupTitle: 'Редактировать профиль',
+    popupPlaceholderUp: 'Введите ФИО',
+    popupPlaceholderDown: 'Кем вы работаете'
+  },
+  {
+    popupTitle: 'Новое место',
+    popupPlaceholderUp: 'Название места',
+    popupPlaceholderDown: 'Ссылка на фото места'
+  }
+];
+
+const arrCardsCaption = [
+  {
+    titlePlace: 'Иркутск',
+    linkPlaceFoto: './images/irkutsk.jpg',
+    altPlaceFoto: 'Памятник Колчаку в г. Иркутск'
+  },
+  {
+    titlePlace: 'Ужур',
+    linkPlaceFoto: './images/uzhur-love.jpg',
+    altPlaceFoto: 'Мозаика - Мы любим Ужур в центре города'
+  },
+  {
+    titlePlace: 'Сибирский',
+    linkPlaceFoto: './images/sibirski.jpg',
+    altPlaceFoto: 'Храм в центре г. Сибирский'
+  },
+  {
+    titlePlace: 'Новосибирск',
+    linkPlaceFoto: './images/novosibirsk.jpg',
+    altPlaceFoto: 'ЖД вокзал г. Новосибирск ночью - весь в снегу'
+  },
+  {
+    titlePlace: 'Омск',
+    linkPlaceFoto: './images/omsk.jpg',
+    altPlaceFoto: 'Зима - багровый закат накрывает мост уходящий в даль, справа - поворачивает дорожная пробка, церковь'
+  },
+  {
+    titlePlace: 'Оренбург',
+    linkPlaceFoto: './images/orenburg.jpg',
+    altPlaceFoto: 'Памятник Гагарину, проложившему дорогу в космос всему человечеству в г. Оренбург'
+  }
+];
+
+
+
+initialCards(); /*отображаем карточки на странице*/
 
 const buttonEdit = document.querySelector('.profile__button-edit');
 const buttonClose = document.querySelector('.popup__button-close');
@@ -85,6 +134,19 @@ function setLikeStatus(event) {
 function openPopupAddPlace() {
   console.log('Нажата кнопка добавить новое место');
   overlayToggle();
+}
+
+function initialCards() { /*Функция к/я добавляет карточки на страницу*/
+  const elementsList = document.querySelector('.elements__list'); /*Получить доступ к элементу ul*/
+  const elementsItemTemplate = document.querySelector('#template__elements-item').content; /*получить доступ к template__elements-item и его содержимому*/
+  /*Наполнить карточку содержимым перебрав все элементы массива*/
+  arrCardsCaption.forEach((item) => {
+    const elementsItem = elementsItemTemplate.querySelector('.elements__item').cloneNode(true); /*клонировать li вместе с содержимым*/
+    elementsItem.querySelector('.element__image').src = item.linkPlaceFoto; /*задать значение параметру src*/
+    elementsItem.querySelector('.element__image').alt = item.altPlaceFoto; /*задать значение параметру alt*/
+    elementsItem.querySelector('.element__title').textContent = item.titlePlace; /*задать значение заголовку h2*/
+    elementsList.append(elementsItem); /*добавить заполненныю карточку на страницу*/
+  });
 }
 
 
