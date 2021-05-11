dca1091196c6fe8de16f790618435fda1e0ff59d
myasoedas <myasoedas@yandex.ru>
2021-05-11 10:38:29 +0300

    fix: Массив с данными по карточкам мест вынес в отдельный файл initial-сards.js и добавил его подключение выше файла index.js, чтобы массив загружался раньше чем функци якоторая его использует

---
+++ scripts/initial-сards.js
@@ -0,0 +1,32 @@
+const arrCardsCaption = [
+  {
+    titlePlace: 'Иркутск',
+    linkPlaceFoto: './images/irkutsk.jpg',
+    altPlaceFoto: 'Памятник Колчаку в г. Иркутск'
+  },
+  {
+    titlePlace: 'Ужур',
+    linkPlaceFoto: './images/uzhur-love.jpg',
+    altPlaceFoto: 'Мозаика - Мы любим Ужур в центре города'
+  },
+  {
+    titlePlace: 'Сибирский',
+    linkPlaceFoto: './images/sibirski.jpg',
+    altPlaceFoto: 'Храм в центре г. Сибирский'
+  },
+  {
+    titlePlace: 'Новосибирск',
+    linkPlaceFoto: './images/novosibirsk.jpg',
+    altPlaceFoto: 'ЖД вокзал г. Новосибирск ночью - весь в снегу'
+  },
+  {
+    titlePlace: 'Омск',
+    linkPlaceFoto: './images/omsk.jpg',
+    altPlaceFoto: 'Зима - багровый закат накрывает мост уходящий в даль, справа - поворачивает дорожная пробка, церковь'
+  },
+  {
+    titlePlace: 'Оренбург',
+    linkPlaceFoto: './images/orenburg.jpg',
+    altPlaceFoto: 'Памятник Гагарину, проложившему дорогу в космос всему человечеству в г. Оренбург'
+  }
+];
