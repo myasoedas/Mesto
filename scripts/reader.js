a3eab73accd7c64c414d798451bf24ed2ad01b67
myasoedas <myasoedas@yandex.ru>
2021-07-06 15:02:07 +0300

    fix: Удалил неиспользуемые файлы js из проекта

--- scripts/reader.js
+++
@@ -1,6 +0,0 @@
-initialCards.forEach((element) => {
-  const card = new Card(element, initialCssClasses, openPopupImage); // создали объект карточки
-  card.setCallbackHandlerOpenPopup(/* здесь функция открытия картинки */); // добавили колбек на открытие картинки
-  const elementsItem = card.createCard(); // сгенерировали DOM-элемент и установили обработчики
-  addCard(elementsItem); // вставили карточку на страницу
-});
